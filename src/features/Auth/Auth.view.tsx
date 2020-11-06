0072bc8ca1d641f80484279dd8ab9f8172d4872f
Nata Entali <nata.entali@protonmail.com>
2020-11-06 22:21:08 +0200

    suncalc library

--- src/features/Auth/Auth.view.tsx
+++ src/features/Auth/Auth.view.tsx
@@ -1,15 +1,27 @@
-import React, { useReducer } from 'react'
+import React, {useEffect, useReducer} from 'react'
 import { setUserAction } from './Auth.actions'
 import { INITIAL_STATE, authReducer } from './Auth.reducer'
 import { Button } from 'primereact/button'
 import { Logo } from '../../components/Logo'
 import { googleAuth } from '../../firebase'
+// @ts-ignore
+import SunCalc from 'suncalc'
 
 const Auth = () => {
   const [dispatch] = useReducer(authReducer, INITIAL_STATE);
   const onClick = () => {
     googleAuth().then((user) => dispatch(setUserAction(user)))
   }
+
+  useEffect(() => {
+    console.log('Moon Position', SunCalc.getMoonPosition(
+        new Date(),
+        50.450100,
+        30.523399
+    ))
+
+    console.log('Moon Illumination', SunCalc.getMoonIllumination(new Date()))
+  })
 
   return (
       <section className="Auth">
