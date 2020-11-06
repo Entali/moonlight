5cff3f904a25c97b62b245cddcaca898bda87b16
Nata Entali <nata.entali@protonmail.com>
2020-11-06 21:27:13 +0200

    Primereact as UI kit

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './firebase'
 import './index.css'
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 ReactDOM.render(
   <React.StrictMode>
