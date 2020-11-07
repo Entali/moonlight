ebf8716f31aef0eb15af2728c7776af9c170e69f
Nata Entali <nata.entali@protonmail.com>
2020-11-07 23:33:20 +0200

    setError action

--- src/features/Auth/Auth.actions.tsx
+++ src/features/Auth/Auth.actions.tsx
@@ -1,8 +1,16 @@
-const setUserAction = (user: any) => ({
+import { UserModel } from './Auth.models'
+
+const setUserAction = (user: UserModel) => ({
   type: 'SET_USER',
   payload: user
 });
 
+const setErrorAction = (error: string) => ({
+  type: 'SET_ERROR',
+  payload: error
+});
+
 export {
-  setUserAction
+  setUserAction,
+  setErrorAction
 }
