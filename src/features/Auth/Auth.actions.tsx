72aa800a4160320b3213b78e04ec815e7a7bc98e
Nata Entali <nata.entali@protonmail.com>
2020-11-22 11:53:59 +0200

    createUserRecord & makeUserProfile as actions, optimizing workflow w/ DB

--- src/features/Auth/Auth.actions.tsx
+++ src/features/Auth/Auth.actions.tsx
@@ -1,3 +1,4 @@
+import { firestore } from '../../firebase'
 import { UserModel } from './Auth.models'
 
 type ErrorType = string | null
@@ -12,7 +13,28 @@ const setErrorAction = (error: ErrorType) => ({
   payload: error
 });
 
+const makeUser = (user: any) => {
+  return user && user.uid && {
+    id: user.uid,
+    name: user.displayName,
+    email: user.email,
+    img: user.photoURL,
+    created: new Date()
+  }
+}
+
+const createUserRecord = async (authMethod: any, dispatch: any) => {
+  if (!authMethod) return
+
+  const userRef = firestore.doc('users/3878327468723648')
+  const snapShot = await userRef.get()
+
+  console.log('snapshot', snapShot)
+}
+
 export {
   setUserAction,
-  setErrorAction
+  setErrorAction,
+  makeUser,
+  createUserRecord
 }
--- src/features/Auth/Auth.view.tsx
+++ src/features/Auth/Auth.view.tsx
@@ -1,49 +1,16 @@
 import React, { useCallback, useReducer } from 'react'
-import { setUserAction, setErrorAction } from './Auth.actions'
+import { createUserRecord } from './Auth.actions'
 import { INITIAL_STATE, authReducer } from './Auth.reducer'
 import { Logo } from '../../components/Logo'
-import { googleAuth } from '../../firebase'
-import { firestore } from '../../firebase'
-import { UserModel } from './Auth.models'
 import Button from '@material-ui/core/Button'
+import { googleAuth } from '../../firebase'
 
 const Auth = () => {
   const [state, dispatch] = useReducer(authReducer, INITIAL_STATE);
 
-  const makeUserRecord = (user: any) => {
-    return user && user.uid && {
-      id: user.uid,
-      name: user.displayName,
-      email: user.email,
-      img: user.photoURL
-    }
-  }
-
-  const onClick = useCallback(async () => {
-    try {
-      const { user } = await googleAuth()
-      const userRecord = await makeUserRecord(user)
-
-      // in some reason there is undefined returns from promise
-      // after setting user to DB
-      await firestore
-          .collection('users')
-          .doc(userRecord.id)
-          .set(userRecord)
-
-      // that's why getter goes right after set
-      const newUser = await firestore
-          .collection('users')
-          .doc(userRecord.id)
-          .get()
-          .then((doc) => doc.data())
-
-      dispatch(setUserAction(newUser as UserModel))
-      dispatch(setErrorAction(null))
-      return newUser
-    } catch (err) {
-      dispatch(setErrorAction(err.message))
-    }
+  const onClick = useCallback(() => {
+    createUserRecord(googleAuth, dispatch)
+        .then(what => console.log('what',what))
   }, [])
 
   return (
