eb0ab374a8d894c0f73e49b514bbd5a205eba068
Nata Entali <nata.entali@protonmail.com>
2020-11-23 13:20:15 +0200

    routing w/ props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { withRouter, RouteComponentProps } from 'react-router'
 import Routes from './router'
 import './App.css'
 
-function App() {
-  return (
-      <section className="App">
-        <Routes/>
-      </section>
-  )
+const INITIAL_STATE = {}
+
+type Props = RouteComponentProps & {}
+
+class App extends Component<Props> {
+  state = INITIAL_STATE
+
+  render() {
+    console.log('App props', this.props)
+    return (
+        <section className="App">
+          <Routes/>
+        </section>
+    )
+  }
 }
 
-export default App
+export default withRouter(App)
--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Auth } from './features/Auth'
+import { About } from './components/About'
 
 const Routes = () => {
   return (
       <Switch>
-        <Route path="/" component={Auth}/>
+        <Route exact path="/" component={Auth}/>
+        <Route exact path="/about" component={About}/>
       </Switch>
   );
 };
