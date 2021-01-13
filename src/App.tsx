d679fe7938909c438ef9775406b27a8a15c4d283
Nata Entali <nata.entali@protonmail.com>
2021-01-13 12:00:02 +0200

    About removed, App added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Routes from './router'
+
+const App = () => {
+  return (
+      <section className="App">
+        <Header/>
+        <Routes/>
+      </section>
+  )
+}
+
+export default App
