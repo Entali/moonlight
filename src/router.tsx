e8cc85cbf2b39f20c9e7cea0a985b0d0c54f8ad6
Nata Entali <nata.entali@protonmail.com>
2021-01-13 11:59:48 +0200

    routes updated

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import {Home} from './components/Home'
-import {About} from './components/About'
+import {SignIn} from './components/SignIn'
 
 const Routes = () => {
   return (
       <Switch>
         <Route exact path="/" component={Home}/>
-        <Route exact path="/about" component={About}/>
+        <Route exact path="/signin" component={SignIn}/>
       </Switch>
   );
 };
